eae8fe723488e14bc9d10ea204f33b12fd01ca97
rabochiy <imolodcov@mail.ru>
2020-10-13 14:10:21 +0300

    2020.10.13 14:10

---
+++ task 1/script.js
@@ -0,0 +1,4 @@
+let button = document.querySelector('.button');//находим кнопку
+button.onclick = function () { //отслеживаем нажатие на кнопку
+    console.log("Клик"); //пишем в консоль сообщение
+}
