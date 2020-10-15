fab7322913dae4ee4f8fb554f79c19b3bb64b0e5
rabochiy <imolodcov@mail.ru>
2020-10-15 14:42:58 +0300

    2020.10.15 14:10

--- task 2/script.js
+++ task 2/script.js
@@ -1,6 +1,6 @@
 let button = document.querySelector('.button');//создаем переменную которая находит кнопку
-let figure = document.querySelector('.figure')//создаем переменную которая находит блок с записаным числом
-score = figure.textContent;// извлекаем текстовое значение из блока
+let figure = document.querySelector('.figure');//создаем переменную которая находит блок с записаным числом
+score = figure.innerHTML;// извлекаем текстовое значение из блока
 score = Number.parseInt(score);//преобразовываем текстовое значение из блока в числовое
 button.onclick = function () {//функция отслеживает нажати кнопки
     score++;//добавляем к числовому значению 1
