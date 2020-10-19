d717b3a3871d6fc71256f5e7730c8e041f2c176a
rabochiy <imolodcov@mail.ru>
2020-10-19 15:26:11 +0300

    2020.10.19 15:25

--- task 7/script.js
+++ task 7/script.js
@@ -1,58 +1,51 @@
-let button = document.querySelector('.button');
+let add__button = document.querySelector('.add__button');
+let add__input = document.querySelector('.add__input');
+let find__button = document.querySelector('.find__button');
+let find__input = document.querySelector('.find__input');
 let table = document.querySelector('table');//создаем переменную которая находит таблицу
-/*let oddNumer = function (someNumber) {//проверяем значения вводимые в ячейку на четностьна четность
-    return (someNumber % 2 === 0);
-}
-*/
+let list = [];
 
 
-let creatArray = function (ValueFromInput) {//создаем массив чисел
-    array = [];
-    array2 = [];
-    for (let i = 1; i <= ValueFromInput; i++) {//Ячейки таблицы
-        for (let j = 1; j <= ValueFromInput; j++)
-        {
-            if (i === j) {
-                array2[j - 1] = 1;
-            } else {
-                array2[j - 1] = 0;
-            }
-            console.log(array2)
-
-        }
-        array[i - 1] = array2;
-    }
-
-}
-
-
-let showArray = function (input) {//создаем таблиццу и заполняем ее из массива
-
-    for (let i = 1; i <= input; i++) {
+let create_table = function () {//создаем таблиццу и заполняем ее из массива
+    let header = document.createElement("th");
+    header.innerText = 'Список';
+    header.className = "td";
+    table.appendChild(header);
+    list.forEach(function (value, index, array) {
         let line = document.createElement("tr");//создаем строку
-        for (let i = 1; i <= input; i++) { //цикл создает ячейки изаполняет их массива
-            let cell = document.createElement("td");//создаем ячейку
-            cell.innerText = array[i - 1];//записываем в ячейку текст из массива
-            /*   if (oddNumer(array[i - 1])) {//присваиваем ячейкам класс в зависимости от четности числа записывемого в него
-                   cell.className = "td td_even";
-               } else {*/
-            cell.className = "td";
-            //   }*/
-            line.appendChild(cell);//добавляем в строку ячейку
-
-        }
-
+        let cell = document.createElement("td");//создаем ячейку
+        cell.innerText = value;//записываем в ячейку текст из массива
+        cell.className = "td";
+        line.appendChild(cell);//добавляем в строку ячейку
         table.appendChild(line);//добавляем  строку в таблицу
-    }
+
+    })
+
+
 }
 
 
-button.onclick = function () {//функция отслеживает нажати кнопки
-    table.innerHTML = '';//очищает таблицу
-    let input = document.querySelector('.input').value;//присваивает значение в поле ввода переменной input
-    creatArray(input);//создаем массив чисел
-    showArray(input);//создаем массив чисел
-    document.querySelector('.input').value = '';//очищает поле ввода
-   // console.log(array)
+find__button.onclick = function () {//функция отслеживает нажати кнопки
+    if (find__input.value === "") {
+        return
+    } else {
+        table.innerHTML = '';//очищает таблицу
+
+        find__input.value = '';//очищает поле ввода
+        create_table();
+
+    }
+}
+
+add__button.onclick = function () {//функция отслеживает нажати кнопки
+    if (add__input.value === "") {
+        return
+    } else {
+        table.innerHTML = '';//очищает таблицу
+        list.push(add__input.value);
+        add__input.value = '';//очищает поле ввода
+        create_table();
+        console.log(list);
+    }
 }
 
---
+++ task 8/script.js
@@ -0,0 +1,41 @@
+let button = document.querySelector('.button');
+let table = document.querySelector('table');//создаем переменную которая находит таблицу
+let inversion = document.querySelector('.inversion');
+let creatArray = function (ValueFromInput) {//создаем массив чисел
+    array = [];
+    for (let i = 1; i <= ValueFromInput; i++) {
+        array2 = [];
+        for (let j = 1; j <= ValueFromInput; j++) {
+            if (i === j) {
+                array2[j - 1] = 1;
+            } else {
+                array2[j - 1] = 0;
+            }
+        }
+        array[i - 1] = array2;
+    }
+}
+let showArray = function (input) {//создаем таблиццу и заполняем ее из массива
+    for (let i = 1; i <= input; i++) {
+        let line = document.createElement("tr");//создаем строку
+        for (let j = 1; j <= input; j++) { //цикл создает ячейки изаполняет их массива
+            let cell = document.createElement("td");//создаем ячейку
+            if (inversion.checked) {
+                cell.innerText = array[input - i][j - 1];//записываем в ячейку текст из массива
+            } else {
+                cell.innerText = array[i - 1][j - 1];
+            }
+            cell.className = "td";
+            line.appendChild(cell);//добавляем в строку ячейку
+        }
+        table.appendChild(line);//добавляем  строку в таблицу
+    }
+}
+button.onclick = function () {//функция отслеживает нажати кнопки
+    table.innerHTML = '';//очищает таблицу
+    let input = document.querySelector('.input').value;//присваивает значение в поле ввода переменной input
+    creatArray(input);//создаем массив чисел
+    showArray(input);//создаем массив чисел
+    document.querySelector('.input').value = '';//очищает поле ввода
+}
+
